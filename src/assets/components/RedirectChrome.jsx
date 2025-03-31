20f4ea5c0f61f9a761be0e9732b5a38604736073
JustindotDev <acebedojustin.ranagan@gmail.com>
2025-03-31 18:36:49 +0800

    change the url for the redirect to chrome components

--- src/assets/components/RedirectChrome.jsx
+++ src/assets/components/RedirectChrome.jsx
@@ -8,7 +8,7 @@ const RedirectToChrome = () => {
     if (userAgent.includes("FBAN") || userAgent.includes("FBAV")) {
       // Redirect to Chrome
       window.location.href =
-        "intent://justindotdev-personal-portfolio.vercel.app/#Intent;scheme=https;package=com.android.chrome;end;";
+        "intent://justindotdev.vercel.app/#Intent;scheme=https;package=com.android.chrome;end;";
     }
   }, []);
 
